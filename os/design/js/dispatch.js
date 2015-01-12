9ef05dca4ccbfdd05fb87eb7976d7d479d132f75
MaureenChan <maureencqm@gmail.com>
2015-01-12 23:18:17 +0800

    add annotation

--- os/design/js/dispatch.js
+++ os/design/js/dispatch.js
@@ -107,15 +107,16 @@ $(document).ready(function () {
 
   function PCB(i) {
     var ret = {};
-    ret.name = 'process' + i;
-    ret.super = 0;
-    ret.ntime = parseInt(Math.random() * 90 + 10);
-    ret.atime = parseInt(Math.random() * 100);
-    ret.stime = 0;
-    ret.rtime = 0;
-    ret.ftime = 0;
-    ret.ttime = 0;
-    ret.wtime = 0;
+    ret.name = 'process' + i;   //进程名
+    ret.super = 0;              //完成次序标记
+    ret.ntime = parseInt(Math.random() * 90 + 10);  //需要运行时间
+    ret.atime = parseInt(Math.random() * 100);      //到达时间
+    ret.stime = 0;  //开始时间
+    ret.rtime = 0;  //执行时间
+    ret.ftime = 0;  //完成时间
+    ret.ttime = 0;  //周转时间
+    ret.wtime = 0;  //带权周转时间
+    //更新表中数据
     ret.update = function (algo) {
       $('#' + algo + this.name).css('width', this.rtime / this.ntime * 100 + '%');
       $('#' + algo + 'tr-' + this.name + ' td')[3].innerHTML = this.stime;
@@ -124,8 +125,8 @@ $(document).ready(function () {
       $('#' + algo + 'tr-' + this.name + ' td')[6].innerHTML = this.ttime;
       $('#' + algo + 'tr-' + this.name + ' td')[7].innerHTML = this.wtime.toFixed(2);
 
-      var avg_ttime = 0;
-      var avg_wtime = 0;
+      var avg_ttime = 0;    //平均周转时间
+      var avg_wtime = 0;    //平均带权周转时间
 
       var ttimes = $('#table-cbs-' + algo + ' .ttime');
       var wtimes = $('#table-cbs-' + algo + ' .wtime');
@@ -144,6 +145,7 @@ $(document).ready(function () {
     };
     return ret;
   }
+  //生成一个pcb
   function createCB(type, count) {
     var ret = [];
     var CB;
@@ -156,12 +158,15 @@ $(document).ready(function () {
     return ret;
   }
 });
+// 按到达时间升序排序
 function com_atime(value1, value2) {
   return value1.atime - value2.atime;
 }
+// 按需要时间升序排序
 function com_ntime(value1, value2) {
   return value1.ntime - value2.ntime;
 }
+// 按剩余工作时间排序
 function com_rtime(value1, value2) {
   a = value1.ntime - value1.rtime;
   b = value2.ntime - value2.rtime;
@@ -172,14 +177,16 @@ function com_rtime(value1, value2) {
   else 
     return 0;
 }
+// 先来先服务算法
 function fcfs(cbs, fre) {
-  var running = null;
-  var ready = [];
-  var finish = [];
-  var step = 0;
-  var i = 1;
-  cbs.sort(com_atime);
+  var running = null;   // 存储正在运行的pcb
+  var ready = [];       // 就绪队列
+  var finish = [];      // 完成队列
+  var step = 0;         // 全局时间控制
+  var i = 1;            // 控制完成次序
+  cbs.sort(com_atime);  // 初始进程队列按照到达时间排序
   var interval = setInterval(function () {
+    //如果达到到达时间，则插入到就绪队列中
     if(cbs.length != 0) {
       while (step == cbs[0].atime) {
         ready.push(cbs.shift());
@@ -189,20 +196,27 @@ function fcfs(cbs, fre) {
     }
 
     if(running == null) {
-      ready.sort(com_atime);
+      // 如果没有程序在运行,则判断就绪队列是否有进程
       if (ready.length == 0){
+        //没有进程则时间向前移
         step++;
       } else if (step >= ready[0].atime) {
+        // 存在进程则把就绪队列按照到达时间排序
+        // 先到达的则先执行
+        ready.sort(com_atime);
         running = ready.shift();
         running.stime = step;
         running.update('fcfs');
       }
     } else {
+      // 如果存在进行的进程的话，则判断是否执行完毕
       if (running.rtime != running.ntime) {
+        // 没有执行完毕则继续执行
         running.rtime++;
         running.update('fcfs');
         step++;
       } else {
+        // 完成了则计算完成时间，周转时间和带权周转时间
         running.ftime = step;
         running.ttime = running.ftime - running.atime;
         running.wtime = running.ttime / running.ntime;
@@ -215,13 +229,14 @@ function fcfs(cbs, fre) {
     }
   }, fre);
 }
+// 非抢占的短进程优先
 function spf(cbs, fre) {
-  var running = null;
-  var ready = [];
-  var finish = [];
-  var step = 0;
-  var i = 1;
-  cbs.sort(com_atime);
+  var running = null;       // 存储正在运行的pcb
+  var ready = [];           // 就绪队列
+  var finish = [];          // 完成队列
+  var step = 0;             // 全局时间控制
+  var i = 1;                // 控制完成次序
+  cbs.sort(com_atime);      // 初始进程队列按照到达时间排序
   var interval = setInterval(function () {
     //如果到达开始时间就进入就绪队列
     if(cbs.length != 0) {
@@ -233,20 +248,27 @@ function spf(cbs, fre) {
     }
 
     if(running == null) {
-      ready.sort(com_ntime);
+      // 如果没有程序在运行,则判断就绪队列是否有进程
       if (ready.length == 0){
+        //没有进程则时间向前移
         step++;
       } else if (step >= ready[0].atime) {
+        // 存在进程则把就绪队列按照需要时间排序
+        // 需要时间短的则先执行
+        ready.sort(com_ntime);
         running = ready.shift();
         running.stime = step;
         running.update('spf');
       }
     } else {
+      // 如果存在进行的进程的话，则判断是否执行完毕
       if (running.rtime != running.ntime) {
+        // 没有执行完毕则继续执行
         running.rtime++;
         running.update('spf');
         step++;
       } else {
+        // 完成了则计算完成时间，周转时间和带权周转时间
         running.ftime = step;
         running.ttime = running.ftime - running.atime;
         running.wtime = running.ttime / running.ntime;
@@ -258,14 +280,16 @@ function spf(cbs, fre) {
     }
   }, fre);
 }
+// 抢占的短进程优先
 function preemptive_spf (cbs, fre) {
-  var running = null;
-  var ready = [];
-  var finish = [];
-  var step = 0;
-  var i = 1;
-  cbs.sort(com_atime);
+  var running = null;           // 存储正在运行的pcb
+  var ready = [];               // 就绪队列
+  var finish = [];              // 完成队列
+  var step = 0;                 // 全局时间控制
+  var i = 1;                    // 控制完成次序
+  cbs.sort(com_atime);          // 初始进程队列按照到达时间排序
   var interval = setInterval(function () {
+    //如果到达开始时间就进入就绪队列
     if(cbs.length != 0) {
       while (step == cbs[0].atime) {
         ready.push(cbs.shift());
@@ -273,25 +297,31 @@ function preemptive_spf (cbs, fre) {
           break;
       }
     }
-    console.log(ready);
-    console.log(finish);
+    // 判断就绪队列是否有进程
     if (ready.length == 0) {
+      //没有进程则时间向前移
       step++;
     } else {
+      // 存在进程则把就绪队列按照剩余需要时间排序
+      // 短的进程则先执行
       ready.sort(com_rtime);
       running = ready.shift();
+      // 判断是否执行过
       if(running.rtime) {
         running.update('preemptive_spf');
       } else {
+        // 没有被执行过则需要初始化开始时间
         running.stime = step;
         running.update('preemptive_spf');
       }
       if (running.rtime != running.ntime) {
+        // 每次执行完一次之后要判断是否有进程来抢占
         running.rtime++;
         running.update('preemptive_spf');
         ready.push(running);
         step++;
       } else {
+        // 完成了则计算完成时间，周转时间和带权周转时间
         running.ftime = step;
         running.ttime = running.ftime - running.atime;
         running.wtime = running.ttime / running.ntime;
@@ -303,14 +333,15 @@ function preemptive_spf (cbs, fre) {
 
   }, fre);
 }
+// 高响应比优先算法
 function hrrn(cbs, fre) {
-  var running = null;
-  var ready = [];
-  var finish = [];
-  var step = 0;
-  var i = 0;
-  cbs.sort(com_atime);
-
+  var running = null;       // 存储正在运行的pcb
+  var ready = [];           // 就绪队列
+  var finish = [];          // 完成队列
+  var step = 0;             // 全局时间控制
+  var i = 0;                // 控制完成次序
+  cbs.sort(com_atime);      // 初始进程队列按照到达时间排序
+  // 计算并优化响应比优先
   function com_rp(value1, value2) {
     a = (step - value1.atime) / value1.ntime;
     b = (step - value2.atime) / value2.ntime;
@@ -322,6 +353,7 @@ function hrrn(cbs, fre) {
       return 0;
   }
   var interval = setInterval(function () {
+    //如果到达开始时间就进入就绪队列
     if(cbs.length != 0) {
       while (step == cbs[0].atime) {
         ready.push(cbs.shift());
@@ -331,20 +363,27 @@ function hrrn(cbs, fre) {
     }
 
     if(running == null) {
-      ready.sort(com_rp);
+      // 如果没有程序在运行,则判断就绪队列是否有进程
       if (ready.length == 0){
+        //没有进程则时间向前移
         step++;
       } else if (step >= ready[0].atime) {
+        // 存在进程则把就绪队列按照响应比逆序排序
+        // 响应比高的则先执行
+        ready.sort(com_rp);
         running = ready.shift();
         running.stime = step;
         running.update('hrrn');
       }
     } else {
+      // 如果存在进行的进程的话，则判断是否执行完毕
       if (running.rtime != running.ntime) {
+        // 没有执行完毕则继续执行
         running.rtime++;
         running.update('hrrn');
         step++;
       } else {
+        // 完成了则计算完成时间，周转时间和带权周转时间
         running.ftime = step;
         running.ttime = running.ftime - running.atime;
         running.wtime = running.ttime / running.ntime;
@@ -356,21 +395,25 @@ function hrrn(cbs, fre) {
     }
   }, fre);
 }
+// 时间片算法
 function rr(cbs, fre) {
-  var ready = [];
-  var running;
-  var finish = [];
-  var i = 1;
-  var step = 0;
-  var len;
-  cbs.sort(com_atime);
+  var running;          // 存储正在运行的pcb
+  var ready = [];       // 就绪队列
+  var finish = [];      // 完成队列
+  var step = 0;         // 全局时间控制
+  var i = 1;            // 控制完成次序
+  var len;              // 存储就绪队列长度
+  cbs.sort(com_atime);  // 初始进程队列按照到达时间排序
   var interval = setInterval(function () {
+    //如果到达开始时间就进入就绪队列
     if(cbs.length != 0) {
       while (step == cbs[0].atime) {
         running = cbs.shift();
         if(ready.length) {
+          // 就绪队列不空则等到前面队列执行之后才执行
           running.stime = step + ready.length;
         } else {
+          // 就绪队列为空则马上执行
           running.stime = step;
         }
         running.update('rr');
@@ -384,6 +427,7 @@ function rr(cbs, fre) {
     if(len){
       running = ready.shift();
       running.update('rr');
+      // 运行完成
       if(running.rtime == running.ntime) {
         running.ftime = step;
         running.ttime = running.ftime - running.atime;
@@ -392,11 +436,13 @@ function rr(cbs, fre) {
         running.update('rr');
         finish.push(running);
       } else {
+        // 还没运行完就运行一次后返回就绪队列
         running.rtime++;
         step++;
         ready.push(running);
       }
     } else {
+      // 没有就绪队列就时间向前移
       step++;
     }
 

812d050ad67e8e2a1791592e278df638a4c0e590
linkkingjay <linkkingjay@gmail.com>
2015-01-18 02:39:39 +0800

    add assets in layout

--- database/design/laravel/app/views/layout.blade.php
+++ database/design/laravel/app/views/layout.blade.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>{{{$title}}}</title>
+    <link rel="stylesheet" href="{{{URL::asset('css/bootstrap.min.css')}}}">
 </head>
 <body>
     <div class="container">
         @yield('content')
     </div>
+    <script src="{{{URL::asset('js/bootstrap.min.js')}}}"></script>
 </body>
 </html>

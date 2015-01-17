4543b4e30a655e63f650f71ffab0945e60d8f4f4
linkkingjay <linkkingjay@gmail.com>
2015-01-18 00:12:01 +0800

    username -> name

--- database/design/laravel/app/views/user/login.blade.php
+++ database/design/laravel/app/views/user/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     {{Form::open(array('url' => 'user/post_login'))}}
         <h3>Login</h3>
-        <input type="text" name="username" placeholder="user name">
+        <input type="text" name="name" placeholder="user name">
         <input type="password" name="password" placeholder="password">
         <input type="submit"  value="login">
     {{Form::close()}}

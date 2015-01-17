90b7d6264fda0e08d965854077987d4d40727f72
linkkingjay <linkkingjay@gmail.com>
2015-01-18 00:22:41 +0800

    login

--- database/design/laravel/app/views/user/register.blade.php
+++ database/design/laravel/app/views/user/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-    {{Form::open(array('url' => 'user/post_login'))}}
+    {{Form::open(array('url' => 'user/post_register'))}}
         <h3>register</h3>
         <input type="text" name="username" placeholder="user name">
         <input type="password" name="password" placeholder="password">

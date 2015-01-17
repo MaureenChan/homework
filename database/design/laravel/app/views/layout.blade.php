bfbf07fd14678618ebdf9c99478cd7230a84d5a7
linkkingjay <linkkingjay@gmail.com>
2015-01-18 02:42:37 +0800

    add jQuery

--- database/design/laravel/app/views/layout.blade.php
+++ database/design/laravel/app/views/layout.blade.php
@@ -9,6 +9,7 @@
     <div class="container">
         @yield('content')
     </div>
+    <script src="{{{URL::asset('js/jquery.min.js')}}}"></script>
     <script src="{{{URL::asset('js/bootstrap.min.js')}}}"></script>
 </body>
 </html>

5be016182d15e5b6e8b6cd6ba8be50eb1548e835
linkkingjay <linkkingjay@gmail.com>
2015-01-18 10:44:23 +0800

    show asker\'s name

--- database/design/laravel/app/views/user/me.blade.php
+++ database/design/laravel/app/views/user/me.blade.php
@@ -5,7 +5,7 @@
     <h2>Questions to answer:</h2>
     @foreach ($questions_to_answer as $question)
         <div>
-            <h4>Q: {{$question->question}}</h4>
+            <h4>Q: {{$question->question}} -- {{$question->asker->name}}</h4>
         </div>
         {{Form::open(array('url' => '/answer', 'class' => 'form-inline', 'id' => 'form-answer'))}}
         <input type="hidden" name="question_id" value="{{{$question->question_id}}}">
@@ -18,7 +18,7 @@
     <h2>Questions answered:</h2>
     @foreach ($questions_answered as $question)
         <div>
-            <h4>Q: {{$question->question}}</h4>
+            <h4>Q: {{$question->question}} -- {{$question->asker->name}}</h4>
             <p>A: {{$question->answer->answer}}</p>
         </div>
     @endforeach
